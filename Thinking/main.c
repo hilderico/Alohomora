65eb9cd7c8055dc22998fc559e9b0d32940ee602
Hilderico Pereira <hilderico.pereira@gmail.com>
2018-12-06 22:34:27 -0300

    não compila que dá erro, tenho que testar função seeking

--- Thinking/main.c
+++ Thinking/main.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "../Boxint/Arquivos.c"
+
+struct strstring{
+    char charstring[200]
+};
 
 int main()
 {
+    char colect[] = "-nome:hilderico pereira de sousa neto-";
+    char nome[200][200];
+    // tem que coletar o nome completo que fica entre ':' e '-'
+    limpa(nome,200);
+    seeking(char seek[],char bloc[200][200])
+
+
+
     printf("Hello world!\n");
     return 0;
 }
